d5dde10d2aa88924a18c246bb5bb5fd4a3d91646
leopheard <leopheard.misc@gmail.com>
2019-08-30 17:13:25 -0400

    upload

--- addon.py
+++ addon.py
@@ -7,14 +7,10 @@ from resources.lib import criminalpodcast
 from xmbc import xmbcplugin
 
 URL = "http://feeds.thisiscriminal.com/CriminalShow"
-
 plugin = Plugin()
 
 @plugin.route('/')
 def main_menu():
-    """
-    main menu 
-    """
     items = [
         {
             'label': plugin.get_string(30001), 
@@ -29,46 +25,27 @@ def main_menu():
             'path': plugin.url_for('new_to_criminal'),
             'thumbnail': "/home/osmc/.kodi/addons/plugin.audio.criminalpodcast/resources/media/cropped-favicon-2-180x180-inverted.png"},
     ]
-
     return items
 
 @plugin.route('/all_episodes/')
 def all_episodes():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup = criminalpodcast.get_soup(URL)
-    
     playable_podcast = criminalpodcast.get_playable_podcast(soup)
-    
     items = criminalpodcast.compile_playable_podcast(playable_podcast)
-
     return items
 
 @plugin.route('/all_episodes1/')
 def all_episodes1():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup = criminalpodcast.get_soup(URL)
-    
     playable_podcast1 = criminalpodcast.get_playable_podcast1(soup)
-    
     items = criminalpodcast.compile_playable_podcast1(playable_podcast1)
-
     return items
 
 @plugin.route('/new_to_criminal/')
 def new_to_criminal():
-    """
-    contains playable podcasts listed as just-in
-    """
     #soup = criminalpodcast.compile_new_to_criminal(URL)
-    
     compile_ntc = criminalpodcast.get_new_to_criminal
-    
     items = criminalpodcast.compile_new_to_criminal(compile_ntc)
-
     return items
 
 if __name__ == '__main__':
